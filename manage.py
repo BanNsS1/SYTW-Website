9019eb7beb8ca16f700332c2dd5b295fc974b6ae
Javier <javi_lcs16@hotmail.com>
2017-04-21 15:53:02 +0200

    Project Created

---
+++ manage.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import os
+import sys
+
+if __name__ == "__main__":
+<<<<<<< HEAD
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "images.settings")
+=======
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "battleNet.settings")
+>>>>>>> 545165cbca60ecbd840e7aa6b6d16cc4b2590188
+
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
